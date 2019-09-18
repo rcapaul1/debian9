c6fa7a89c1727248cdcaf179a48b9164f1676d81
Rogé Capaul <rcapaul@hosttech.ch>
2019-09-18 15:36:11 +0200

    new file:   gameserver/ark-server-auto.sh

---
+++ gameserver/ark-server-auto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+adduser arkserver
+dpkg --add-architecture i386; apt update; apt install mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 libstdc++6:i386 -y
+
+cd /home/arkserver
+su arkserver -c "wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh arkserver"
+su arkserver -c "./arkserver install"
